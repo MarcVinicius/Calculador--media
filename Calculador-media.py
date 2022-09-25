586800b4ada831656260ca7a16989a9ffe2bea6b
MarcVinicius <viniciusmarcos.01@hotmail.com>
2022-09-24 21:14:29 -0300

    Ćodigo ganhou funcionalidade

--- Calculador-media.py
+++ Calculador-media.py
@@ -1,1 +1,26 @@
-print("Olá, mundo")
+vezes = 0
+soma = 0
+nums = []
+resposta = 'S'
+
+while True:
+    if resposta == 'S':
+        if vezes == 0:
+            num = input("Digite o primeiro número: ")
+            vezes += 1
+            nums.append(num)
+        elif vezes > 0:
+            num = input("Digite o próximo número: ")
+            vezes += 1
+            nums.append(num)
+        
+        resp = input("deseja adicionar mais um número? Responda com S ou N: ")
+        if resp == 'N':
+            resposta = 'N'
+    else:
+        break
+
+for i in nums:
+    soma += int(i)
+
+print("Sua média é", (soma/len(nums)))
